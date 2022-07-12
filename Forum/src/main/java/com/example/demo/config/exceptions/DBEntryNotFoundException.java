df74616994912e082000176188138472ac6a6c36
MaksimKosyhin <104004024+MaksimKosyhin@users.noreply.github.com>
2022-07-12 22:47:25 +0300

    Delete Forum directory

--- Forum/src/main/java/com/example/demo/config/exceptions/DBEntryNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.config.exceptions;
-
-public class DBEntryNotFoundException extends RuntimeException{
-	private static final long serialVersionUID = 4341521579523553238L;
-
-	public DBEntryNotFoundException(String message) {
-		super(message);
-	}
-}
